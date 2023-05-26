c65f4ddbd39603cd819749cfd86d5a679bb3cf90
MISAGITHAY <125407240+Rabelaqbv@users.noreply.github.com>
2023-05-26 15:50:47 -0300

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,1 +1,20 @@
+fun main() {
+  println("Olá Mundo")
+  var nome= "Isabelly"
+val nascimento = 2005
 
+println(nome)           // exibe o nome 
+println(nascimento)      // exibe o nascimento
+val myNum = 5             // Int
+val myDoubleNum = 5.99    // Double
+val myLetter = 'D'        // Char
+val myBoolean = true      // Boolean
+val C = "Olá"       // String
+
+println (myNum)
+println (myDoubleNum)
+println (myLetter)
+println (myBoolean)
+println (myBoolean)
+}
+  
